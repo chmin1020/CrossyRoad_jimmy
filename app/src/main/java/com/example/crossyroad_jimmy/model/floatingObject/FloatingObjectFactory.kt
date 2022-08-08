66dac0f142a17a0f4ea619d56d10febbe128c0f7
germanJM <chmin1020@gmail.com>
2022-08-08 17:16:56 +0900

    Delete FloatingObjectFactory.kt

--- app/src/main/java/com/example/crossyroad_jimmy/model/floatingObject/FloatingObjectFactory.kt
+++
@@ -1,8 +0,0 @@
-package com.example.crossyroad_jimmy.model.floatingObject
-
-import com.example.crossyroad_jimmy.model.ObjectSize
-import com.example.crossyroad_jimmy.model.Position
-
-interface FloatingObjectFactory {
-    fun create(id: Long, position: Position, velocity: Float, size: ObjectSize): FloatingObject
-}
